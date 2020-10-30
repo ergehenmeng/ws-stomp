e6d13937246b1945fd86269bf90cc62d95390d87
殿小二 <dianxiaoer@mandofin.com>
2020-10-30 10:39:17 +0800

    修改代码

--- src/main/java/com/eghm/websocket/listener/SystemContextListener.java
+++ src/main/java/com/eghm/websocket/listener/SystemContextListener.java
@@ -3,24 +3,25 @@ package com.eghm.websocket.listener;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+
 /**
  * 系统启动监听器
- * @author Administrator
  *
+ * @author Administrator
  */
-public class SystemContextListener implements ServletContextListener{
+public class SystemContextListener implements ServletContextListener {
 
     @Override
-	public void contextInitialized(ServletContextEvent sce) {
-		ServletContext context = sce.getServletContext();
-		context.setAttribute("cxtPath", context.getContextPath());
-		
-	}
+    public void contextInitialized(ServletContextEvent sce) {
+        ServletContext context = sce.getServletContext();
+        context.setAttribute("cxtPath", context.getContextPath());
+
+    }
 
     @Override
-	public void contextDestroyed(ServletContextEvent sce) {
+    public void contextDestroyed(ServletContextEvent sce) {
 
-	}
+    }
 
 
 }
