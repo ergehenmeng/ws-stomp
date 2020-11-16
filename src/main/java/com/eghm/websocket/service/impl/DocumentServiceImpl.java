889b5d65d813696f8d932cb0a378127d0e6fc7ad
殿小二 <dianxiaoer@mandofin.com>
2020-11-16 19:37:39 +0800

    优化代码

--- src/main/java/com/eghm/websocket/service/impl/DocumentServiceImpl.java
+++ src/main/java/com/eghm/websocket/service/impl/DocumentServiceImpl.java
@@ -4,6 +4,7 @@ import com.eghm.websocket.dto.request.SearchDocumentRequest;
 import com.eghm.websocket.enums.ErrorCode;
 import com.eghm.websocket.enums.FileType;
 import com.eghm.websocket.exception.SystemException;
+import com.eghm.websocket.mapper.DocumentMapper;
 import com.eghm.websocket.model.Document;
 import com.eghm.websocket.service.DocumentService;
 import com.eghm.websocket.utils.KeyGenerator;
