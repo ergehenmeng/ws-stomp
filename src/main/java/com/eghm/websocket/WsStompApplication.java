af89e9d6b3c7b86a8eee1a2f5e63a02f962f98b9
殿小二 <dianxiaoer@mandofin.com>
2021-06-15 16:26:25 +0800

    Lifecycle接口

--- src/main/java/com/eghm/websocket/WsStompApplication.java
+++ src/main/java/com/eghm/websocket/WsStompApplication.java
@@ -1,18 +1,45 @@
 package com.eghm.websocket;
 
+import com.eghm.websocket.utils.SpringUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.SmartLifecycle;
+import org.springframework.context.event.ContextRefreshedEvent;
 
 /**
  * @author 二哥很猛
  */
 @SpringBootApplication
 @MapperScan(basePackages = "com.eghm.websocket.mapper")
-public class WsStompApplication {
+@Slf4j
+public class WsStompApplication implements ApplicationListener<ContextRefreshedEvent>, SmartLifecycle {
 
     public static void main(String[] args) {
-        SpringApplication.run(WsStompApplication.class, args);
+        new SpringApplicationBuilder().sources(WsStompApplication.class).run(args);
     }
-
+    
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        ApplicationContext context = event.getApplicationContext();
+        SpringUtil.setApplicationContext(context);
+    }
+    
+    @Override
+    public void start() {
+        log.info("我启动了....");
+    }
+    
+    @Override
+    public void stop() {
+    
+    }
+    
+    @Override
+    public boolean isRunning() {
+        return false;
+    }
 }
